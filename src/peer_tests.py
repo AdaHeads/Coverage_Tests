b484da920d1f6fa2747c4e7befd2c85178c40314
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-02-18 12:49:38 +0100

    Removed potential race condition that caused tests to fail. Now, every tests will try to use a new agent.

--- src/peer_tests.py
+++ src/peer_tests.py
@@ -2,7 +2,7 @@ import logging
 import config
 from call_flow_communication import callFlowServer
 
-from sip_profiles import agent1107
+from sip_profiles import agent1103
 from sip_utils import SipAccount, SipAgent
 
 try:
@@ -14,23 +14,23 @@ logging.basicConfig(level=logging.INFO)
 
 class PeerTests(unittest.TestCase):
 
-    cfs = callFlowServer(uri=config.call_flow_server_uri, authtoken=agent1107.authtoken)
+    cfs = callFlowServer(uri=config.call_flow_server_uri, authtoken=agent1103.authtoken)
     # Tests whether a call-flow server responds correctly to peer registrations
     # 
     def testRegisterAgent(self):
-        sipagent = SipAgent(account=SipAccount(username=agent1107.username, password=agent1107.password, sip_port=agent1107.sipport))
+        sipagent = SipAgent(account=SipAccount(username=agent1103.username, password=agent1103.password, sip_port=agent1103.sipport))
     
-        peer = self.cfs.peerList().locatePeer(agent1107.username)
+        peer = self.cfs.peerList().locatePeer(agent1103.username)
         if peer ['registered']:
-            self.fail("Peer seems to be already registered: " + peer.toString())
+            self.fail("Peer seems to be already registered: " + str (peer))
         
         sipagent.Connect()
-        peer = self.cfs.peerList().locatePeer(agent1107.username)
+        peer = self.cfs.peerList().locatePeer(agent1103.username)
         if not peer ['registered']:
-            self.fail("Peer does not seem to be registered: " + peer.toString())
+            self.fail("Peer does not seem to be registered: " + str (peer))
         
         sipagent.QuitProcess()
-        peer = self.cfs.peerList().locatePeer(agent1107.username)
+        peer = self.cfs.peerList().locatePeer(agent1103.username)
         if peer ['registered']:
-            self.fail("Peer is still registered: " + peer.toString())
+            self.fail("Peer is still registered: " + str (peer))
     
