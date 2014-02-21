225a095b147c85cd158537be56da7c82539f0787
Jacob Sparre Andersen <jsa@adaheads.com>
2014-02-21 12:04:52 +0000

    Adjusted receptionist ID's to match the test system setup.

--- src/sip_profiles.py
+++ src/sip_profiles.py
@@ -3,67 +3,67 @@ class agent1100:
     password  = '1234'
     sipport   = "5160"
     authtoken = "feedabbadeadbeef0"
-    ID        = 0
+    ID        = 10
 
 class agent1101:
     username  = '1101'
     password  = '1234'
     sipport   = "5161"
     authtoken = "feedabbadeadbeef1"
-    ID        = 1
+    ID        = 11
 
 class agent1102:
     username  = '1102'
     password  = '1234'
     sipport   = "5162"
     authtoken = "feedabbadeadbeef2"
-    ID        = 2
+    ID        = 12
 
 class agent1103:
     username  = '1103'
     password  = '1234'
     sipport   = "5163"
     authtoken = "feedabbadeadbeef3"
-    ID        = 3
+    ID        = 13
 
 class agent1104:
     username  = '1104'
     password  = '1234'
     sipport   = "5164"
     authtoken = "feedabbadeadbeef4"
-    ID        = 4
+    ID        = 14
 
 class agent1105:
     username  = '1105'
     password  = '1234'
     sipport   = "5165"
     authtoken = "feedabbadeadbeef5"
-    ID        = 5
+    ID        = 15
     
 class agent1106:
     username  = '1106'
     password  = '1234'
     sipport   = "5166"
     authtoken = "feedabbadeadbeef6"
-    ID        = 6
+    ID        = 16
 
 class agent1107:
     username  = '1107'
     password  = '1234'
     sipport   = "5167"
     authtoken = "feedabbadeadbeef7"
-    ID        = 7
+    ID        = 17
 
 class agent1108:
     username  = '1108'
     password  = '1234'
     sipport   = "5168"
     authtoken = "feedabbadeadbeef8"
-    ID        = 8
+    ID        = 18
 
 class agent1109:
     username  = '1109'
     password  = '1234'
     sipport   = "5169"
     authtoken = "feedabbadeadbeef9"
-    ID        = 9
+    ID        = 19
