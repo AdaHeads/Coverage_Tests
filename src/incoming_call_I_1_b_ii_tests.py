a01bb2db58ea24abf192ff04f4c06c61d71bc522
Jacob Sparre Andersen <jsa@adaheads.com>
2014-02-21 14:12:32 +0000

    Attempting harder to clear out 'basic_agent's.

--- src/incoming_call_I_1_b_ii_tests.py
+++ src/incoming_call_I_1_b_ii_tests.py
@@ -129,7 +129,9 @@ class Sequence_Diagram (unittest.TestCase):
                 self.fail ("Reception information missing 'greeting'.")
         
     def test_Run (self):
-        call (["killall", "basic_agent"])
+        call (["killall",       "basic_agent"])
+        sleep (0.1)
+        call (["killall", "-9", "basic_agent"])
         sleep (1.0) # Letting the system clean up and settle after the previous test run.
 
         Client = EventListenerThread (uri   = config.call_flow_events,
