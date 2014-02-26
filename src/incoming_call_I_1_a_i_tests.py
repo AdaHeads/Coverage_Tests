72f8a8358d8701a58a14ddfc63db839455bb9b53
Jacob Sparre Andersen <jsa@adaheads.com>
2014-02-26 14:50:20 +0000

    Incoming call use-case sequence diagram test: Propagated changes.

--- src/incoming_call_I_1_a_i_tests.py
+++ src/incoming_call_I_1_a_i_tests.py
@@ -1,4 +1,4 @@
-# https://github.com/AdaHeads/Hosted-Telephone-Reception-System/wiki/Use-case%3A-Indg%C3%A5ende-opkald#wiki-variant-i1a1
+# https://github.com/AdaHeads/Hosted-Telephone-Reception-System/wiki/Use-case%3A-Indg%C3%A5ende-opkald#wiki-variant-i1ai-1
 
 from incoming_calls          import Test_Case
 from sip_profiles            import agent1102 as Caller
@@ -17,12 +17,12 @@ class Sequence_Diagram (Test_Case):
             self.Step (Message = "FreeSWITCH: checks dial-plan => to queue")
             self.Step (Message = "FreeSWITCH->Call-Flow-Control: call queued with dial-tone")
             self.Step (Message = "FreeSWITCH: pauses dial-plan processing for # seconds")
-            Reception_ID = self.Call_Announced ()
+            Call_ID, Reception_ID = self.Call_Announced ()
             self.Step (Message = "Client-N->Receptionist-N: shows call (with dial-tone)")
             Reception_Data = self.Request_Information (Reception_ID = Reception_ID)
-            self.Offers_To_Answer_Call (Call_Flow_Control = self.Call_Flow_Control,
-                                        Reception_ID      = Reception_ID)
-            Call_Information = self.Call_Allocation_Acknowledgement (Reception_ID    = Reception_ID,
+            self.Offer_To_Pick_Up_Call (Call_Flow_Control = self.Call_Flow_Control,
+                                        Call_ID           = Call_ID)
+            Call_Information = self.Call_Allocation_Acknowledgement (Call_ID         = Call_ID,
                                                                      Receptionist_ID = Receptionist.ID)
             self.Step (Message = "Client-N->Receptionist-N: Information on JSA R&I (with full greeting).")
             self.Step (Message = "Call-Flow-Control->FreeSWITCH: connect call to phone-N")
