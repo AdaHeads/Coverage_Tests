f8cb949fc09d8228597164fcb5730d5c9dc4d0a1
Jacob Sparre Andersen <jsa@adaheads.com>
2014-03-03 12:31:34 +0000

    Using agent pools.

--- src/incoming_call_I_1_b_i_tests.py
+++ src/incoming_call_I_1_b_i_tests.py
@@ -1,16 +1,12 @@
 # https://github.com/AdaHeads/Hosted-Telephone-Reception-System/wiki/Use-case%3A-Indg%C3%A5ende-opkald#wiki-variant-i1bi-1
 
-from incoming_calls          import Test_Case
-from sip_profiles            import agent1106 as Caller
-from sip_profiles            import agent1109 as Receptionist
-from config                  import queued_reception as Reception
+from incoming_calls import Test_Case
+from config         import queued_reception as Reception
 
 class Sequence_Diagram (Test_Case):
     def test_Run (self):
         try:
-            self.Preconditions (Caller       = Caller,
-                                Receptionist = Receptionist,
-                                Reception    = Reception)
+            self.Preconditions (Reception = Reception)
 
             self.Caller_Places_Call ()
             self.Caller_Hears_Dialtone ()
@@ -21,18 +17,17 @@ class Sequence_Diagram (Test_Case):
             self.Step (Message = "Client-N->Receptionist-N: shows call (with dial-tone)")
             self.Step (Message = "Receptionist-N->Client-N: state-switch-free")
             Reception_Data = self.Request_Information (Reception_ID = Reception_ID)
-            self.Offer_To_Pick_Up_Call (Call_Flow_Control = self.Call_Flow_Control,
+            self.Offer_To_Pick_Up_Call (Call_Flow_Control = self.Receptionist.call_control,
                                         Call_ID           = Call_ID)
             Call_Information = self.Call_Allocation_Acknowledgement (Call_ID         = Call_ID,
-                                                                     Receptionist_ID = Receptionist.ID)
+                                                                     Receptionist_ID = self.Receptionist.ID)
             self.Step (Message = "Client-N->Receptionist-N: Information on JSA R&I (with full greeting).")
             self.Step (Message = "Call-Flow-Control->FreeSWITCH: connect call to phone-N")
             self.Receptionist_Answers (Call_Information      = Call_Information,
                                        Reception_Information = Reception_Data,
                                        After_Greeting_Played = False)
 
-            self.Client.stop ()
+            self.Postprocessing ()
         except:
-            if not self.Client == None:
-                self.Client.stop ()
+            self.Postprocessing ()
             raise
