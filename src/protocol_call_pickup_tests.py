2bac08b57cb41884af5050157978fd2456c51c2c
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-03-03 15:46:26 +0100

    Added checks for correct receptionist ID.

--- src/protocol_call_pickup_tests.py
+++ src/protocol_call_pickup_tests.py
@@ -24,8 +24,8 @@ class Pickup(unittest.TestCase):
         reception_id = 1
         reception = "1234000" + str(reception_id)
 
-        test_receptionist = Receptionists.request()
-        test_customer     = Customers.request()
+        receptionist = Receptionists.request()
+        customer     = Customers.request()
 
         try:
 
@@ -33,31 +33,39 @@ class Pickup(unittest.TestCase):
 
             # Make a call into the reception
             self.log.info ("Spawning a single call to the reception at " + reception)
-            test_customer.sip_phone.Dial(reception)
-            test_receptionist.event_stack.WaitFor(event_type="call_offer")
+            customer.sip_phone.Dial(reception)
+            receptionist.event_stack.WaitFor(event_type="call_offer")
 
-            call = test_receptionist.call_control.PickupCall()
+
+            call = receptionist.call_control.PickupCall()
             if call['reception_id'] != reception_id:
                 self.fail ("Invalid reception ID in allocated call.")
 
+            if call['assigned_to'] != receptionist.ID:
+                self.fail ("Invalid receptionist ID in allocated call.")
+
             self.log.info ("Got call " + call['id'] + " waiting for transfer..")
-            test_receptionist.event_stack.WaitFor(event_type="call_pickup",
+            receptionist.event_stack.WaitFor(event_type="call_pickup",
                                                   call_id=call['id'])
+            pickup_event = receptionist.event_stack.Get_Latest_Event (Event_Type  = "call_pickup",
+                                                                      Destination = reception)
+            if pickup_event['call']['assigned_to'] != receptionist.ID:
+                self.fail ("Invalid receptionist ID in allocated call.")
 
-            test_receptionist.call_control.HangupCall (call_id=call['id'])
+            receptionist.call_control.HangupCall (call_id=call['id'])
 
 
-            test_receptionist.event_stack.WaitFor(event_type="call_hangup", call_id=call['id'])
+            receptionist.event_stack.WaitFor(event_type="call_hangup", call_id=call['id'])
 
             # Check that no calls are in the call list.
-            current_call_list = test_receptionist.call_control.CallList()
+            current_call_list = receptionist.call_control.CallList()
             if not current_call_list.Empty():
                 self.fail("Non-empty call list: " + str (current_call_list ))
-            test_receptionist.release()
-            test_customer.release()
+            receptionist.release()
+            customer.release()
         except:
-            test_receptionist.release()
-            test_customer.release()
+            receptionist.release()
+            customer.release()
             raise
 
     def test_pickup_specified(self):
