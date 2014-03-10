c8a5d746ff43b11034255a8861703cfc1d832503
Jacob Sparre Andersen <jsa@adaheads.com>
2014-03-10 10:48:55 +0000

    Generated integration tests for the find contact use-case (basically a null-test at the moment). + Parent class for the find contact test cases.

---
+++ src/find_contact.py
@@ -0,0 +1,46 @@
+import logging
+from time       import clock, sleep
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+logging.basicConfig (level = logging.INFO)
+
+class Test_Case (unittest.TestCase):
+    Start_Time         = None
+    Next_Step          = 1
+
+    def Preconditions (self, Reception):
+        self.Start_Time = clock ()
+        self.Next_Step  = 1
+
+        self.Log ("Find contact test case: Setting up preconditions...")
+
+    def Postprocessing (self):
+        self.Log ("Find contact test case: Cleaning up after test...")
+
+    def Step (self,
+              Message,
+              Delay_In_Seconds = 0.0):
+        if self.Next_Step is None:
+            self.Next_Step = 1
+        if self.Start_Time is None:
+            self.Start_Time = clock ()
+
+        logging.info ("Step " + str (self.Next_Step) + "@" + str (clock() - self.Start_Time) + ": " + Message)
+        sleep (Delay_In_Seconds)
+        self.Next_Step = self.Next_Step + 1
+
+    def Log (self,
+             Message,
+             Delay_In_Seconds = 0.0):
+        if self.Next_Step is None:
+            self.Next_Step = 1
+        if self.Start_Time is None:
+            self.Start_Time = clock ()
+
+        logging.info ("     " + str (self.Next_Step - 1) + "@" + str (clock() - self.Start_Time) + ": " + Message)
+        sleep (Delay_In_Seconds)
+
---
+++ src/find_contact_1_tests.py
@@ -0,0 +1,21 @@
+# https://github.com/AdaHeads/Hosted-Telephone-Reception-System/wiki/Use-case:-Finde-en-kontakt#wiki-variant-1-1
+
+from find_contact import Test_Case
+
+class Sequence_Diagram (Test_Case):
+    def test_Run (self):
+        try:
+            self.Preconditions ()
+
+            self.Step (Message = "Receptionist-N     ->> Klient-N          [genvejstast-for-kontaktliste]")
+            self.Step (Message = "Receptionist-N    <<-  Klient-N          [fokus: kontaktliste og soegefelt]")
+            self.Step (Message = "=== loop ===")
+            self.Step (Message = "Receptionist-N     ->> Klient-N          [arrow up/down]")
+            self.Step (Message = "Receptionist-N    <<-  Klient-N          [update contact view]")
+            self.Step (Message = "=== end loop ===")
+
+            self.Postprocessing ()
+        except:
+            self.Postprocessing ()
+            raise
+
