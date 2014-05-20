ce9236e3cfdabf50578652fc136f0689128f4923
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-05-20 14:41:57 +0200

    Changed to use dict constructor and added extension field.

--- src/sip_profiles.py
+++ src/sip_profiles.py
@@ -1,187 +1,37 @@
 ReceptionistConfigs = [
-                {
-                 'username'     : '1100',
-                 'password'     : '1234',
-                 'sipport'      : 5160,
-                 'authtoken'    : "feedabbadeadbeef0",
-                 'ID'           : 10,
-                 'receptionist' : True
-                },
-                 {
-                  'username'  : '1101',
-                  'password'  : '1234',
-                  'sipport'   : 5161,
-                  'authtoken' : "feedabbadeadbeef1",
-                  'ID'        : 11,
-                  'receptionist' : True
-                 },
-                 {
-                  'username'  : '1102',
-                  'password'  : '1234',
-                  'sipport'   : 5162,
-                  'authtoken' : "feedabbadeadbeef2",
-                  'ID'        : 12,
-                  'receptionist' : True
-                 },
-                 {
-                  'username'  : '1103',
-                  'password'  : '1234',
-                  'sipport'   : 5163,
-                  'authtoken' : "feedabbadeadbeef3",
-                  'ID'        : 13,
-                  'receptionist' : True
-                 },
-                 {
-                  'username'  : '1104',
-                  'password'  : '1234',
-                  'sipport'   : 5164,
-                  'authtoken' : "feedabbadeadbeef4",
-                  'ID'        : 14,
-                  'receptionist' : True
-                 },
-                 {
-                  'username'  : '1105',
-                  'password'  : '1234',
-                  'sipport'   : 5165,
-                  'authtoken' : "feedabbadeadbeef5",
-                  'ID'        : 15,
-                  'receptionist' : True
-                 },
-                 {
-                  'username'  : '1106',
-                  'password'  : '1234',
-                  'sipport'   : 5166,
-                  'authtoken' : "feedabbadeadbeef6",
-                  'ID'        : 16,
-                  'receptionist' : True
-                 },
-                 {
-                  'username'  : '1107',
-                  'password'  : '1234',
-                  'sipport'   : 5167,
-                  'authtoken' : "feedabbadeadbeef7",
-                  'ID'        : 17,
-                  'receptionist' : True
-                 },
-                 {
-                  'username'  : '1108',
-                  'password'  : '1234',
-                  'sipport'   : 5168,
-                  'authtoken' : "feedabbadeadbeef8",
-                  'ID'        : 18,
-                  'receptionist' : True
-                 },
-                 {
-                  'username'  : '1109',
-                  'password'  : '1234',
-                  'sipport'   : 5169,
-                  'authtoken' : "feedabbadeadbeef9",
-                  'ID'        : 19,
-                  'receptionist' : True
-                 }
+    dict(username='1100', password='1234', sipport=5160, authtoken="feedabbadeadbeef0", ID=10, receptionist=True, extension="1100"),
+    dict(username='1101', password='1234', sipport=5161, authtoken="feedabbadeadbeef1", ID=11, receptionist=True, extension="1101"),
+    dict(username='1102', password='1234', sipport=5162, authtoken="feedabbadeadbeef2", ID=12, receptionist=True, extension="1102"),
+    dict(username='1103', password='1234', sipport=5163, authtoken="feedabbadeadbeef3", ID=13, receptionist=True, extension="1103"),
+    dict(username='1104', password='1234', sipport=5164, authtoken="feedabbadeadbeef4", ID=14, receptionist=True, extension="1104"),
+    dict(username='1105', password='1234', sipport=5165, authtoken="feedabbadeadbeef5", ID=15, receptionist=True, extension="1105"),
+    dict(username='1106', password='1234', sipport=5166, authtoken="feedabbadeadbeef6", ID=16, receptionist=True, extension="1106"),
+    dict(username='1107', password='1234', sipport=5167, authtoken="feedabbadeadbeef7", ID=17, receptionist=True, extension="1107"),
+    dict(username='1108', password='1234', sipport=5168, authtoken="feedabbadeadbeef8", ID=18, receptionist=True, extension="1108"),
+    dict(username='1109', password='1234', sipport=5169, authtoken="feedabbadeadbeef9", ID=19, receptionist=True, extension="1109")
                 ]
 
 CustomerConfigs = [
-                {
-                 'username'  : '1200',
-                 'password'  : '1234',
-                 'sipport'   : 5260,
-                },
-                 {
-                  'username'  : '1201',
-                  'password'  : '1234',
-                  'sipport'   : 5261,
-                 },
-                 {
-                  'username'  : '1202',
-                  'password'  : '1234',
-                  'sipport'   : 5262,
-                 },
-                 {
-                  'username'  : '1203',
-                  'password'  : '1234',
-                  'sipport'   : 5263,
-                 },
-                 {
-                  'username'  : '1204',
-                  'password'  : '1234',
-                  'sipport'   : 5264,
-                 },
-                 {
-                  'username'  : '1205',
-                  'password'  : '1234',
-                  'sipport'   : 5265,
-                 },
-                 {
-                  'username'  : '1206',
-                  'password'  : '1234',
-                  'sipport'   : 5266,
-                 },
-                 {
-                  'username'  : '1207',
-                  'password'  : '1234',
-                  'sipport'   : 5267,
-                 },
-                 {
-                  'username'  : '1208',
-                  'password'  : '1234',
-                  'sipport'   : 5268,
-                 },
-                 {
-                  'username'  : '1209',
-                  'password'  : '1234',
-                  'sipport'   : 5269,
-                 },
-                 {
-                  'username'  : '1210',
-                  'password'  : '1234',
-                  'sipport'   : 5270,
-                 },
-                 {
-                  'username'  : '1211',
-                  'password'  : '1234',
-                  'sipport'   : 5271,
-                 },
-                 {
-                  'username'  : '1212',
-                  'password'  : '1234',
-                  'sipport'   : 5272,
-                 },
-                 {
-                  'username'  : '1213',
-                  'password'  : '1234',
-                  'sipport'   : 5273,
-                 },
-                 {
-                  'username'  : '1214',
-                  'password'  : '1234',
-                  'sipport'   : 5274,
-                 },
-                 {
-                  'username'  : '1215',
-                  'password'  : '1234',
-                  'sipport'   : 5275,
-                 },
-                 {
-                  'username'  : '1216',
-                  'password'  : '1234',
-                  'sipport'   : 5276,
-                 },
-                 {
-                  'username'  : '1217',
-                  'password'  : '1234',
-                  'sipport'   : 5277,
-                 },
-                 {
-                  'username'  : '1218',
-                  'password'  : '1234',
-                  'sipport'   : 5278,
-                 },
-                 {
-                  'username'  : '1219',
-                  'password'  : '1234',
-                  'sipport'   : 5279,
-                 }
+    dict(username='1200', password='1234', sipport=5260, extension="port5260"),
+    dict(username='1201', password='1234', sipport=5261, extension="port5261"),
+    dict(username='1202', password='1234', sipport=5262, extension="port5262"),
+    dict(username='1203', password='1234', sipport=5263, extension="port5263"),
+    dict(username='1204', password='1234', sipport=5264, extension="port5264"),
+    dict(username='1205', password='1234', sipport=5265, extension="port5265"),
+    dict(username='1206', password='1234', sipport=5266, extension="port5266"),
+    dict(username='1207', password='1234', sipport=5267, extension="port5267"),
+    dict(username='1208', password='1234', sipport=5268, extension="port5268"),
+    dict(username='1209', password='1234', sipport=5269, extension="port5269"),
+    dict(username='1210', password='1234', sipport=5270, extension="port5270"),
+    dict(username='1211', password='1234', sipport=5271, extension="port5271"),
+    dict(username='1212', password='1234', sipport=5272, extension="port5272"),
+    dict(username='1213', password='1234', sipport=5273, extension="port5273"),
+    dict(username='1214', password='1234', sipport=5274, extension="port5274"),
+    dict(username='1215', password='1234', sipport=5275, extension="port5275"),
+    dict(username='1216', password='1234', sipport=5276, extension="port5276"),
+    dict(username='1217', password='1234', sipport=5277, extension="port5277"),
+    dict(username='1218', password='1234', sipport=5278, extension="port5278"),
+    dict(username='1219', password='1234', sipport=5279, extension="port5279")
                 ]
 
 class agent1100:
